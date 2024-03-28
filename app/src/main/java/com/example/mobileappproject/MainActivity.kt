c30aed6473d2bc690bce283fef8cc76b2daf09c0
MudassarStd <mudassarstd@gmail.com>
2024-03-28 12:57:40 +0500

    Test Commit

--- app/src/main/java/com/example/mobileappproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileappproject/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        print("checking pushes")
     }
 }
