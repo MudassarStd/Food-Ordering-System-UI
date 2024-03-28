05de41284c6122db68d9d99f2e6203276724a714
MudassarStd <mudassarstd@gmail.com>
2024-03-28 13:04:26 +0500

    Test Commit

--- app/src/main/java/com/example/mobileappproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileappproject/MainActivity.kt
@@ -16,8 +16,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        print("checking pushes")
+        print("checking local branch")
     }
 }
