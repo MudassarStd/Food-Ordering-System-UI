b0dc3003d5d5043d4fa09b138176b18c0963f5da
SyedUmairGardezi <162674959+UmairShah746@users.noreply.github.com>
2024-04-09 02:43:42 +0500

    order completion screen and partial processing screen

---
+++ app/src/main/java/com/example/mobileappproject/delivery/ProcessOrdering.kt
@@ -0,0 +1,21 @@
+package com.example.mobileappproject.delivery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mobileappproject.R
+
+class ProcessOrdering : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_process_ordering)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
