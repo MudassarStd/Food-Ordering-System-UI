c7561e4c4df56841113abb9c4f5a3e0ed1fc78e0
MudassarStd <mudassarstd@gmail.com>
2024-04-09 16:18:49 +0500

    Tried to get Desired Behavior

--- app/src/main/java/com/example/mobileappproject/delivery/ProcessOrdering.kt
+++ app/src/main/java/com/example/mobileappproject/delivery/ProcessOrdering.kt
@@ -6,16 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mobileappproject.R
+import com.example.mobileappproject.databinding.ActivityProcessOrderingBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ProcessOrdering : AppCompatActivity() {
+    private lateinit var binding :  ActivityProcessOrderingBinding
+
+    val fragObject  = OrderProcessingFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_process_ordering)
+
+        binding = ActivityProcessOrderingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        fragObject.show(supportFragmentManager,fragObject.tag)
     }
+    
 }
