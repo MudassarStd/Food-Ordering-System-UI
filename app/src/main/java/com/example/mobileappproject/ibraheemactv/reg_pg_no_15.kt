5d6c22229f1ceb87e557174cfa14a999047a5df5
MudassarStd <mudassarstd@gmail.com>
2024-04-13 00:57:07 +0500

    Some Changes

---
+++ app/src/main/java/com/example/mobileappproject/ibraheemactv/reg_pg_no_15.kt
@@ -0,0 +1,10 @@
+package com.example.mobileappproject
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reg_pg_no_15 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_pg_no15)
+    }
+}
